34b18d04f9dcf78b4d0d095b0f1bcedcde2a357d
dylanito2 <dylanito2@gmail.com>
2017-04-30 00:25:12 -0400

    Done.

--- lib/format_minutes.rb
+++ lib/format_minutes.rb
@@ -1,5 +1,11 @@
 def time_formatter(minutes)
   minutes_left = minutes % 60
   hours = (minutes - minutes_left) / 60
-  "#{hours} hour(s), #{minutes_left} minute(s)"
+  if minutes_left == 0
+    "#{hours} hour(s)"
+  elsif hours == 0
+    "#{minutes_left} minute(s)"
+  else
+    "#{hours} hour(s), #{minutes_left} minute(s)"
+  end
 end
