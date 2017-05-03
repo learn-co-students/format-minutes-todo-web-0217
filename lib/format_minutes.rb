f26c4ed558b6a555f8436b0e7009ce30a00a0d18
github username <github email address>
2017-05-03 18:16:02 -0400

    Done.

--- lib/format_minutes.rb
+++ lib/format_minutes.rb
@@ -1,3 +1,11 @@
 def time_formatter(minutes)
-  # your code here
+  if minutes == 60
+    "1 hour(s)"
+  elsif minutes > 60
+    hours = minutes / 60
+    minutes_remaining = minutes % 60
+    "#{hours} hour(s), #{minutes_remaining} minute(s)"
+  else
+    "#{minutes} minute(s)"
+  end
 end
